e6f957e4f49720ca220722acbc5cfd496e9f539f
kajiharuhyyy <kajiharu0921@gmail.com>
2025-12-02 01:48:46 +0900

    微修正作成

--- src/main/java/com/example/demo/web/StaffController.java
+++ src/main/java/com/example/demo/web/StaffController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.domain.Staff;
@@ -53,5 +54,47 @@ public class StaffController {
         return "redirect:/staffs";
     }  // 新規スタッフを作成するためのメソッド
 
+    @GetMapping("/staffs/{id}/edit")
+    public String editStaffForm(@PathVariable Long id, Model model) {
+        Staff staff = staffRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid staff id " + id));
 
+        StaffForm form = new StaffForm();
+        form.setId(staff.getId());
+        form.setName(staff.getName());
+        form.setEmail(staff.getEmail());
+        form.setActive(staff.isActive());
+
+        model.addAttribute("staffForm", form);
+        return "staffs/edit";
+    }
+
+
+    @PostMapping("/staffs/{id}/edit")
+    public String upDateStaff(@PathVariable Long id,
+            @Valid @ModelAttribute StaffForm staffForm,
+            BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+                return "staffs/edit";
+        }
+         
+        Staff staff = staffRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid staff id " + id));
+
+        staff.setName(staffForm.getName());
+        staff.setEmail(staffForm.getEmail());
+        staff.setActive(staffForm.getActive());
+
+        staffRepository.save(staff);
+
+        return "redirect:/staffs";
+    }
+
+            
+        @PostMapping("/staffs/{id}/delete")
+        public String deleteStaff(@PathVariable Long id) {
+            staffRepository.deleteById(id);
+            return "redirect:/staffs";
+        }
 }
